1674acec77d02b6760a08e59e246447b926b9725
dev <Bichk_di78@student.itstep.org>
2023-05-04 20:52:40 +0300

    lesson_3

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-class Circle:
-    def __init__(self , radius):
-        self.radius = radius
-    def area(self):
-        return 3.14 * (self.radius ** 2)
-name = Circle(32)
-print("Радіус:" , name.radius)
-print("Площа:" , name.area())
+class Aminals:
+    pass
+class Dog(Aminals):
+    def sound(self):
+        print("Гав")
+bobik  = Dog()
+bobik.sound()
