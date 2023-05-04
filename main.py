ff4af8e2192d738a3e2e7a75a47a65479f48f532
dev <Bichk_di78@student.itstep.org>
2023-05-04 20:47:44 +0300

    lesson_2

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-class Student:
-    def __init__(self , name, age):
-        self.name = name
-        self.age = age
-    def info(self):
-        print(f"Ім'я: {self.name} "
-              f"\nРік: {self.age}")
-student = Student("Захар" , 15)
-student.info()
+class Circle:
+    def __init__(self , radius):
+        self.radius = radius
+    def area(self):
+        return 3.14 * (self.radius ** 2)
+name = Circle(32)
+print("Радіус:" , name.radius)
+print("Площа:" , name.area())
